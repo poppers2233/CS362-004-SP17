d987684d1e07532d3b9459142a1d9100a1aafcf9
poppers2233 <jmugica69@gmail.com>
2017-04-30 23:56:01 -0700

    Add files via upload

---
+++ projects/mugicaj/dominion/cardtest1.c
@@ -0,0 +1,47 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+
+
+int main(){
+
+	int r;
+        int check;
+	int count=0;
+        printf("smithy unit test ----\n");
+        int k[10] = {adventurer, council_room, feast, gardens, mine,
+               remodel, smithy, village, baron, great_hall};
+
+        struct gameState G;
+
+         r = initializeGame(2, k, 1, &G);
+	
+	check = cardEffect(smithy,0,0,0,&G,0,0);
+	// one card discarded 3 added
+	int i;
+	for(i=0;i<10; i++){
+		printf("%i\n",G.hand[0][i]);
+		if(G.hand[0][i] > 0){
+			count++;
+		}
+	
+	}
+	if(count == 7){
+	
+		printf("PASS\n");
+		return 0;
+		
+	}
+	
+ 	else{
+		printf("FAILED\n");
+		return -1;
+	}
+	
+	return 0;
+	 
+}
